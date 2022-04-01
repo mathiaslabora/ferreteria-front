e277781466490e70ddaae85463a803b14ca064f2
mathias labora martirena <mathiaslabora@gmail.com>
2022-04-01 20:37:07 -0300

    agregado cli

--- src/components/Venta.js
+++ src/components/Venta.js
@@ -9,28 +9,49 @@ const Venta = () => {//traigo datos del store con el dispatch:
 
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(fetchAllArticulos(), fetchAllPersonas());
+    dispatch(fetchAllArticulos());
   }, [dispatch])
 
+
+  useEffect(() => {
+    dispatch(fetchAllPersonas());
+  }, [dispatch])
   //para el select
   const selectListA = useRef(null);
+  const selectListCli = useRef(null);
 
 
   //setArticulos("gola")
   list.map(a => selectListA.current.innerHTML += `<option value=${a.articulo}/>${a.articulo}</option>`)
+  listP.map(a => selectListCli.current.innerHTML += `<option value=${a.nombre}/>${a.nombre}</option>`)
   //datal.current.innerHTML=articulos;
 
-  console.log(selectListA.current)
+  console.log(selectListCli.current)
   return (
     <div className="container">
-      <div className='row'>
-        <div className='col-4'>
+      <div className='row justify-content-center'>
+        
+        <div className='col-2'>
           <h1>Facturacion</h1>
+          {/* articulos */}
+          <form className='form'>
           <label className="form-label">Articulos:</label>
           <select ref={selectListA}>
             <option>Escoja un articulo</option>
           </select>
+          <button>Agregar</button>
+          
+{/* cliente */}
+
+<label className="form-label">Cliente:</label>
+          <select ref={selectListCli}>
+            <option>Seleccione cliente</option>
+          </select>
+          </form>
         </div>
+        
+        
+
       </div>
     </div>
 
