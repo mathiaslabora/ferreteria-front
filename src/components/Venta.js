a5394112e1d8a390ee093544d34e990fa7bdf55c
mathias labora martirena <mathiaslabora@gmail.com>
2022-04-01 22:54:57 -0300

    agregado cant articulos

--- src/components/Venta.js
+++ src/components/Venta.js
@@ -22,49 +22,64 @@ const Venta = () => {//traigo datos del store con el dispatch:
   const listToAdd = useRef(null);
 
   //setArticulos("gola")
-  list.map(a => selectListA.current.innerHTML += `<option value=${a.articulo}/>${a.articulo}</option>`)
-  listP.map(a => selectListCli.current.innerHTML += `<option value=${a.nombre}/>${a.nombre}</option>`)
+  list.map(a => selectListA.current.innerHTML += `<option value=${a.articulo}>${a.articulo}</option>`)
+  listP.map(a => selectListCli.current.innerHTML += `<option value=${a.nombre}>${a.nombre}</option>`)
   //datal.current.innerHTML=articulos;
+  let objArt = []
+  const cantidad = useRef(null);
 
-  const addArt = () => {
+  const costo = useRef(null);
+
+
+  const addArt = (e) => {
+    e.preventDefault();
+    listToAdd.current.innerHTML = ``
+    objArt.push({
+      articulo: selectListA.current.value,
+      costo: costo.current.value,
+      cantidad: cantidad.current.value
+    })
+    objArt.map(a => listToAdd.current.innerHTML += `<li>${a.articulo} - ${a.costo} - ${a.cantidad}</li>`)
+    console.log(objArt)
+  }
+
+  const submitFact = () => {
 
   }
 
-
-  console.log(selectListCli.current)
   return (
-  
-      <div className='row justify-content-center'>
 
-        <div className='col-5'>
-          <h1>Facturacion</h1>
+    <div className='row justify-content-center'>
 
-          <form>
-            {/* cliente */}
-            <label className="form-label">Cliente:</label>
-            <select className="form-select" id="cliente" ref={selectListCli}>
-              <option>Seleccione cliente</option>
-            </select>
-            {/* articulos */}
-            <label className="form-label">Articulos:</label>
-            <select className="form-select" ref={selectListA}>
-              <option>Escoja un articulo</option>
-            </select>
-            <label>Costo unitario</label>
-            <input className="form-control" id='costo' placeholder='Pesos'></input>
-            {/* cantidad */}
-            <label>Cantidad</label>
-            <input className="form-control" id='cantidad' placeholder='Cantidad'></input>
-            <button className="btn btn-primary btn-sm mb-3 mt-3" onClick={addArt} >Agregar Producto</button>
-            <textarea placeholder='Descripcion' className="form-control" ></textarea>
-            <button className='btn btn-danger btn-lg mb-3 mt-3 '>Facturar</button>
-          </form>
-        </div>
+      <div className='col-5'>
+        <h1>Facturacion</h1>
 
-
+        <form onSubmit={submitFact}>
+          {/* cliente */}
+          <label className="form-label">Cliente:</label>
+          <select className="form-select" id="cliente" ref={selectListCli}>
+           
+          </select>
+          {/* articulos */}
+          <label className="form-label">Articulos:</label>
+          <select className="form-select" ref={selectListA}>
+            
+          </select>
+          <label>Costo unitario</label>
+          <input className="form-control" ref={costo} placeholder='Pesos'></input>
+          {/* cantidad */}
+          <label>Cantidad</label>
+          <input className="form-control" ref={cantidad} placeholder='Cantidad'></input>
+          <button className="btn btn-primary btn-sm mb-3 mt-3" onClick={addArt} >Agregar Producto</button>
+          <textarea placeholder='Descripcion' className="form-control" ></textarea>
+          <button className='btn btn-danger btn-lg mb-3 mt-3 '>Facturar</button>
+        </form>
+      </div>
+      <div className='col-5'>
         <div ref={listToAdd}></div>
       </div>
-    
+    </div>
+
 
 
 
